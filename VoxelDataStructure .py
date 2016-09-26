181dd8fcfcc0f294e4cef7879c66f0b23cc9e009
NJ <nj.namju@gmail.com>
2016-09-25 20:21:52 -0400

    update

---
+++ VoxelDataStructure .py
@@ -0,0 +1,32 @@
+import Rhino as r # using RhinoCommon
+
+class VoxelMap():
+    pixels = []
+    def Build(self):
+        pass
+
+class Pixel():
+    p0 = [];
+    p1 = [];
+    pmid = [];
+    i = -1; 
+    j = -1;
+    k = -1;
+    npix = []
+
+    occupancy = 0.0;
+    occupancyTemp = 0.0;
+    friction = 0.0;
+    isSolid = False;
+    isFixed = False;
+    staticFlow = [0,0,0]
+    flow = [0,0,0]
+
+    def GetCenterPt(self):
+        return r.Geometry.Point3d(self.pmid[0], self.pmid[1], self.pmid[2])
+
+thePixel = Pixel()
+thePixel.pmid = [1,2,0]
+
+a = thePixel.GetCenterPt();
+
